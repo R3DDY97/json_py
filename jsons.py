136bfa59df27a96511f13a2a9310934985819438
R3DDY97 <venkyreddy097@gmail.com>
2018-03-01 15:53:17 +0530

    added sample jsons to test

--- jsons.py
+++ jsons.py
@@ -2,6 +2,8 @@
 '''converts JSON into python objects'''
 
 import string
+import os
+from pprint import pprint
 
 DIGITS = set(string.digits)
 DIGITS_ = set(string.digits).union({"-"})
@@ -26,53 +28,30 @@ def null_parser(json_str):
 
 def whitespace_parser(json_str):
     index = 0
-    total_consumed = ""
-
     while json_str[index] in WHITESPACES:
-        total_consumed += json_str[index]
         index += 1
-    return (total_consumed, json_str[index:])
-
-def comma_parser(json_str):
-    if json_str[0] == ",":
-        return (json_str[0], json_str[1:])
-    return None
-
+    return json_str[index:]
 
 def space_comma_parser(json_str):
     index = 0
-    total_consumed = ""
-
     while json_str[index] in WHITESPACES:
-        total_consumed += json_str[index]
         index += 1
-
     if json_str[index] == ',':
-        total_consumed += ","
         index += 1
-
     while json_str[index] in WHITESPACES:
-        total_consumed += json_str[index]
         index += 1
-    return (total_consumed, json_str[index:])
+    return json_str[index:]
 
 
 def space_colon_parser(json_str):
     index = 0
-    total_consumed = ""
-
     while json_str[index] in WHITESPACES:
-        total_consumed += json_str[index]
         index += 1
-
     if json_str[index] == ':':
-        total_consumed += ":"
         index += 1
-
     while json_str[index] in WHITESPACES:
-        total_consumed += json_str[index]
         index += 1
-    return (total_consumed, json_str[index:])
+    return json_str[index:]
 
 def string_parser(json_str):
     index = 0
@@ -141,33 +120,20 @@ def num_parser(json_str):
     except ValueError:
         return (float(number), json_str[index:])
 
-
-    # try:
-    #
-    #     if isinstance(int(number), int):
-    #         return (int(number), json_str[index:])
-    #     elif isinstance(float(number), float):
-    #         return (float(number), json_str[index:])
-    # except ValueError:
-    #     return (json_str[:index], json_str[index:])
-
-
 def array_parser(json_str):
 
     array_list = []
 
     if json_str[0] == "[" and json_str[1] == "]":
         return (array_list, json_str[2:])
-    if json_str[0] == "[":
+    elif json_str[0] == "[":
         json_str = json_str[1:]
     else:
         return None
 
-    if whitespace_parser(json_str):
-        _, json_str = whitespace_parser(json_str)
+    json_str = whitespace_parser(json_str)
 
     while json_str[0] != "]":
-
         parser_output = value_parser(json_str)
         if parser_output:
             value, json_str = parser_output
@@ -175,13 +141,12 @@ def array_parser(json_str):
         else:
             break
 
-        if whitespace_parser(json_str):
-            _, json_str = whitespace_parser(json_str)
+        json_str = whitespace_parser(json_str)
 
         if json_str[0] == "]":
             break
         elif space_comma_parser(json_str):
-            _, json_str = space_comma_parser(json_str)
+            json_str = space_comma_parser(json_str)
     return (array_list, json_str[1:])
 
 def object_parser(json_str):
@@ -196,8 +161,7 @@ def object_parser(json_str):
         return None
 
     while json_str[0] != "}":
-        if whitespace_parser(json_str):
-            _, json_str = whitespace_parser(json_str)
+        json_str = whitespace_parser(json_str)
 
         if string_parser(json_str):
             key, json_str = string_parser(json_str)
@@ -205,7 +169,7 @@ def object_parser(json_str):
             break
 
         if space_colon_parser(json_str):
-            _, json_str = space_colon_parser(json_str)
+            json_str = space_colon_parser(json_str)
         else:
             return None
 
@@ -215,22 +179,22 @@ def object_parser(json_str):
         else:
             return None
 
-        if whitespace_parser(json_str):
-            _, json_str = whitespace_parser(json_str)
+        json_str = whitespace_parser(json_str)
 
         if json_str[0] == "}":
             break
 
         elif space_comma_parser(json_str):
-            _, json_str = space_comma_parser(json_str)
+            json_str = space_comma_parser(json_str)
 
     return (object_as_dict, json_str[1:])
 
 
 
 def value_parser(json_str):
-    if whitespace_parser(json_str):
-        _, json_str = whitespace_parser(json_str)
+    ''' main function which calls the other subparser functions taking input JSON string'''
+
+    json_str = whitespace_parser(json_str)
 
     parsers = [bool_parser, null_parser, num_parser,
                string_parser, array_parser, object_parser]
@@ -239,3 +203,20 @@ def value_parser(json_str):
         result = parser(json_str)
         if result:
             return result
+    return None
+
+
+def main():
+    '''runs json parser from input json file'''
+    jsfile = os.path.abspath(os.sys.argv[1])
+    if os.path.isfile(os.path.abspath(jsfile)):
+        json_str = ''
+        with open(jsfile, "r") as json_content:
+            for line in json_content:
+                json_str += line
+    parser_ouput = value_parser(json_str)[0]
+    pprint(parser_ouput)
+    return parser_ouput
+
+if __name__ == '__main__':
+    main()
