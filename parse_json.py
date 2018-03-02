543de39602dcbb1da10e1ca74a90f5275c3c554d
R3DDY97 <venkyreddy097@gmail.com>
2018-03-02 12:10:47 +0530

    added load n loads functionality

--- parse_json.py
+++ parse_json.py
@@ -2,30 +2,24 @@
 '''converts JSON into python objects'''
 
 import os
-from pprint import pprint
 import jsons
 
 
-
-def load():
+def load(jsfile):
     '''runs json parser from input json file'''
-    jsfile = os.path.abspath(os.sys.argv[1])
-    if os.path.isfile(os.path.abspath(jsfile)):
-        json_str = ''
-        with open(jsfile, "r") as json_content:
-            for line in json_content:
-                json_str += line
+    # jsfile = os.path.abspath(os.sys.argv[1])
+    if not os.path.isfile(os.path.abspath(jsfile)):
+        print("\nCant find the file\n")
+        return None
+    json_str = ''
+    with open(jsfile, "r") as json_content:
+        for line in json_content:
+            json_str += line
     parser_ouput = jsons.value_parser(json_str)[0]
-    pprint(parser_ouput)
     return parser_ouput
 
 
-def loads(json_str):
+def loads(json_string):
     '''directly parses JSON input string'''
-    parser_ouput = jsons.value_parser(json_str)[0]
-    pprint(parser_ouput)
+    parser_ouput = jsons.value_parser(json_string)[0]
     return parser_ouput
-
-
-if __name__ == '__main__':
-    load()
